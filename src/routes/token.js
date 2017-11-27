181af4fbf1d2ddca8cde13f25ee5506cf900135c
Ilnur <ilnur.a.g@gmail.com>
2017-11-27 21:21:17 +0300

    btc correct

--- src/routes/token.js
+++ src/routes/token.js
@@ -269,7 +269,7 @@ function stats(req, res){
     const nox2btc = nox2eth.mul(exch.btc);
     const nox2usd = nox2eth.mul(exch.usd);
     
-    const btc = btc2big(satRaised);
+    const btc = btc2big(satRaised).add('2.10757488');
     const eth = wei2big(weiRaised).add('570.1777172255927');
     const usd = btc.div(exch.btc).mul(exch.usd).add(eth.mul(exch.usd));  
 
